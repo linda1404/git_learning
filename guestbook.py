42b9d2d48dc94947000a209b4b3f956a4874a73b
[linda <[lindaz.1404@gmail.com]>
2017-09-01 07:25:12 +1000

    Added variable

--- guestbook.py
+++ guestbook.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return '<h1>Hello there!<h1>'
-@app.route('/home')
-def home():
-	return '<h1> You are on the home page<h1>'
+
+# Adding in variables into the route name
+@app.route('/home/<place>')
+def home(place):
+	return '<h1> You are on the home' + place + ' page<h1>'
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1',debug=True, port = 5001)
